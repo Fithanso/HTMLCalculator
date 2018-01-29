2a1c77bebd24742ecfd9b8b25754f678e6167ad3
Fithanso <fms160602@gmail.com>
2018-01-29 13:14:00 +0300

    clear function added

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,50 @@
 require([], function () {
-    var a = document.getElementById('add');
-    var b = document.getElementById('minus');
-    var c = document.getElementById('multi');
-    var d = document.getElementById('break');
-    var e = document.getElementById('super');
-    var i = document.getElementById('summ');
 
-    a.onclick = function() {
-    	
-        var first = parseInt(prompt("Первое слагаемое"));
-        var second = parseInt(prompt("Второе слагаемое"));
-        var total = first + second;
-        i.innerHTML = total;
+    var buttons = document.getElementsByTagName("button");
+    for(var i=0; i<buttons.length; i++) {
+        var button = buttons[i];
+        if (button.innerHTML == "=") {
+            button.addEventListener("click", equals, false);
+        }else if(button.innerHTML == "Clear") {
+            button.addEventListener("click", clear, false);
+        }else if(button.innerHTML == "R") {
+            button.addEventListener("click", clear_one, false);
+        }else{
+            button.addEventListener("click", numclick, false);
+        }
+    }
+    var display = document.getElementById("display");
+    var result = false;
+    var clickres = false;
+
+
+    function equals () {
+        var value = eval(display.innerHTML);
+        display.innerHTML = value;
+        result = true;
     }
 
-    b.onclick = function() {
-    
-    	var first = parseInt(prompt("Из чего вычитаем"));
-        var second = parseInt(prompt("Что вычитаем"));
-        var total = first - second;
-        i.innerHTML = total;
+    function numclick () {
+
+        if (result == true) {
+            result = false;
+            display.innerHTML = ""
+        }
+        display.innerHTML+=this.innerHTML;
+        clickres = true;
     }
 
-        c.onclick = function() {
-       
-    	var first = parseInt(prompt("Что умножаем"));
-        var second = parseInt(prompt("На что умножаем"));
-        var total = first * second;
-        i.innerHTML = total;
-    }
+   function clear () {
+       if(clickres == true) {
+           clickres = false;
+           display.innerHTML = " "
+       }
+   }
 
-        d.onclick = function() {
-        
-    	var first = parseInt(prompt("Что делим"));
-        var second = parseInt(prompt("На что делим"));
-        var total = first / second;
-        i.innerHTML = total;
-    }
-
-        e.onclick = function() {
-        
-    	var first = parseInt(prompt("Основание"));
-        var second = parseInt(prompt("Степень"));
-        var total = Math.pow(first, second);
-        i.innerHTML = total;
-    }
-    
+   function clear_one () {
+       var arr = [display.innerHTML];
+       arr.pop();/*не работает как надо*/
+   }
 });
 
 
